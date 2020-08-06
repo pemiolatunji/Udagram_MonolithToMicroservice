1a2d3297dfd2b52fdd9987a4a6d87e0a4cda8628
priyankasharma972 <62941317+priyankasharma972@users.noreply.github.com>
2020-08-06 21:17:40 +0200

    final changes

--- udagram-api-user/src/config/config.ts
+++ udagram-api-user/src/config/config.ts
@@ -1,15 +1,14 @@
 export const config = {
-  "username": "prishardbnew",
-  "password": "prishardbnew",
-  "database": "prishardbnew",
-  "host": "prishar.czy0l1iexu0j.us-east-1.rds.amazonaws.com",
-  "dialect": "postgres",
-  "aws_region": "us-east-1",
-  "aws_profile": "default",
-  "aws_media_bucket": "prisharbucket-dev",
-  "url": " http://localhost:8100",
-  "jwt": {
-    "secret": "hello"
-  }
-
+  'username': process.env.POSTGRES_USERNAME,
+  'password': process.env.POSTGRES_PASSWORD,
+  'database': process.env.POSTGRES_DB,
+  'host': process.env.POSTGRES_HOST,
+  'dialect': 'postgres',
+  'aws_region': process.env.AWS_REGION,
+  'aws_profile': process.env.AWS_PROFILE,
+  'aws_media_bucket': process.env.AWS_BUCKET,
+  'url': process.env.URL,
+  'jwt': {
+    'secret': process.env.JWT_SECRET,
+  },
 };
